9b658295b0a9b2982ebf8a17ecdb6c388842471c
winniesheng <ewinnie.sheng@gmail.com>
2024-08-15 18:49:13 +0800

    add favicon

--- src/config.ts
+++ src/config.ts
@@ -25,11 +25,11 @@ export const siteConfig: SiteConfig = {
     }
   },
   favicon: [    // Leave this array empty to use the default favicon
-    // {
-    //   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-    //   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-    //   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-    // }
+    {
+       src: '/favicon/avatar-32.jpg',    // Path of the favicon, relative to the /public directory
+       theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
+       sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
+    }
   ]
 }
 
@@ -48,10 +48,10 @@ export const navBarConfig: NavBarConfig = {
 
 export const profileConfig: ProfileConfig = {
   avatar: 'assets/images/demo-avatar.png',  // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-  name: 'Lorem Ipsum',
-  bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+  name: 'Winnie Sheng',
+  bio: 'Neutrinos are unstoppable!',
   links: [
-    {
+    /*{
       name: 'Twitter',
       icon: 'fa6-brands:twitter',       // Visit https://icones.js.org/ for icon codes
                                         // You will need to install the corresponding icon set if it's not already included
@@ -62,11 +62,11 @@ export const profileConfig: ProfileConfig = {
       name: 'Steam',
       icon: 'fa6-brands:steam',
       url: 'https://store.steampowered.com',
-    },
+    },*/
     {
       name: 'GitHub',
       icon: 'fa6-brands:github',
-      url: 'https://github.com/saicaca/fuwari',
+      url: 'https://github.com/princesswinnie1122',
     },
   ],
 }
